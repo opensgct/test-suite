684ebce428e0444f06d8b0c2efcfc4067843cdf6
Alexander Bock <mail@alexanderbock.eu>
2019-10-31 18:10:32 +0100

    Initial commit of testing script and testing configuration file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # test-suite
 Configurations, meshs, and reference images for a test suite to ensure that SGCT continues working
+
+Usage:
+./run-test.ps1 -application 'path to test runner'
+
+The test-runner is an application that is build as an Example in the main SGCT repository
---
+++ run-test.ps1
@@ -0,0 +1,38 @@
+param (
+  [Parameter(Mandatory=$true)][string]$application = "test-runner.exe"
+)
+
+if (!(Test-Path -Path $application -PathType Leaf)) {
+  Write-Output "Could not find application $application"
+}
+
+# Create new comparison directory
+if (Test-Path -Path "comparison") {
+  Remove-Item -Path "comparison" -Recurse
+}
+New-Item -Path . -Name "comparison" -ItemType "directory" | Out-Null
+
+# Copy over the reference images
+Write-Output "Copying reference images..."
+Copy-Item -Path "reference/*" -Destination "comparison" -Recurse
+# Rename reference images by adding a "-ref" to the end
+Get-ChildItem -Path "comparison" -File | Rename-Item -NewName {$_.name -replace ".png", "-ref.png" }
+# Create the test images
+Write-Output "Creating test images..."
+Start-Process -FilePath $application -ArgumentList "-config full_test.xml -local 1 --slave"
+Start-Process -FilePath $application -ArgumentList "-config full_test.xml" -Wait
+# Copy current files into comparison folder
+Move-Item -Path "SGCT*.png" -Destination "comparison"
+
+Write-Output "Comparing..."
+Get-ChildItem -Path "comparison" -File -Name | ForEach-Object {
+    if (!($_ -match "-ref")) {
+        $f = $_
+        $f_hash = Get-FileHash "comparison/$f"
+        $ref = $_ -replace ".png", "-ref.png"
+        $ref_hash = Get-FileHash("comparison/$ref")
+        if (!($f_hash.hash -eq $ref_hash.hash)) {
+            Write-Output "\tHash changed: $f"
+        }
+    }
+}
